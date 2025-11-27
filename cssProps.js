5135abd327c40771ff5366ff2c700ffc3432bccf
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-11-27 17:27:24 -0500

    improve element selector

--- cssProps.js
+++ cssProps.js
@@ -1,20 +1,20 @@
 const cssConfig = {
-	color: { type: 'color', value: '#000000' },
-	'background-color': { type: 'color', value: '#ffffff' },
-	'font-size': { type: 'px', value: 16 },
-	'font-family': { type: 'dropdown', value: '', options: ['', 'Arial', 'Verdana', 'Tahoma', 'Times New Roman', 'Courier New', 'monospace'] },
-	'font-weight': { type: 'dropdown', value: '500', options: ['500', '600', '700', 'bold', 'bolder'] },
-	position: { type: 'dropdown', value: 'default', options: ['default', 'static', 'relative', 'absolute'] },
-        top: { type: 'px', value: '', unlocked: { position: ['absolute', 'relative'] } },
-        left: { type: 'px', value: '', unlocked: { position: ['absolute', 'relative'] } },
-        right: { type: 'px', value: '', unlocked: { position: ['absolute', 'relative'] } },
-        bottom: { type: 'px', value: '', unlocked: { position: ['absolute', 'relative'] } },
-	boxShadowX: { type: 'px', value: 0 },
-	boxShadowY: { type: 'px', value: 0 },
-	boxShadowBlur: { type: 'px', value: 0 },
-	boxShadowColor: { type: 'color', value: '#000000' },
-	padding: { type: 'px', value: 0 },
-	margin: { type: 'px', value: 0 }
+	color: { type: 'color' },
+	'background-color': { type: 'color' },
+	'font-size': { type: 'px' },
+	'font-family': { type: 'dropdown', options: ['', 'Arial', 'Verdana', 'Tahoma', 'Times New Roman', 'Courier New', 'monospace'] },
+	'font-weight': { type: 'dropdown', options: ['500', '600', '700', 'bold', 'bolder'] },
+	position: { type: 'dropdown', options: ['default', 'static', 'relative', 'absolute'] },
+        top: { type: 'px', unlocked: { position: ['absolute', 'relative'] } },
+        left: { type: 'px', unlocked: { position: ['absolute', 'relative'] } },
+        right: { type: 'px', unlocked: { position: ['absolute', 'relative'] } },
+        bottom: { type: 'px', unlocked: { position: ['absolute', 'relative'] } },
+	boxShadowX: { type: 'px' },
+	boxShadowY: { type: 'px' },
+	boxShadowBlur: { type: 'px' },
+	boxShadowColor: { type: 'color' },
+	padding: { type: 'px' },
+	margin: { type: 'px' }
 };
 
 
@@ -22,13 +22,11 @@ function rgbToHex(rgb) {
 	const result = /^rgba?\((\d+),\s*(\d+),\s*(\d+)/.exec(rgb);
 	if (!result) return rgb;
 	return (
-		'#' +
-		[1, 2, 3]
-			.map(i => {
-				const hex = parseInt(result[i]).toString(16);
-				return hex.length === 1 ? '0' + hex : hex;
-			})
-			.join('')
+		'#' + [1, 2, 3].map(i => {
+			const hex = parseInt(result[i]).toString(16);
+			return hex.length === 1 ? '0' + hex : hex;
+		})
+		.join('')
 	);
 }
 
@@ -40,7 +38,7 @@ function safePx(val, def) {
 }
 
 function parseInitialStyles(styleString, config = cssConfig) {
-	const props = JSON.parse(JSON.stringify(config));
+	const props = {};
 	const styleMap = {};
 	if (styleString) {
 		styleString.split(';').forEach(pair => {
@@ -51,33 +49,46 @@ function parseInitialStyles(styleString, config = cssConfig) {
 	}
 
     Object.keys(config).forEach(key => {
+		props[key] = { ...config[key] };
+		
 		if (config[key].type === 'color') {
-			let val = styleMap[key] !== undefined ? styleMap[key] : config[key].value;
-			if (val && val.startsWith('rgb')) val = rgbToHex(val);
-			props[key].value = val;
+			let val = styleMap[key];
+			if (val) {
+				if (val.startsWith('rgb')) val = rgbToHex(val);
+				props[key].value = val;
+			} else {
+				props[key].value = '';
+			}
 		} else if (config[key].type === 'dropdown') {
-			props[key].value = styleMap[key] !== undefined ? styleMap[key] : config[key].value;
+			if (styleMap[key] !== undefined) {
+				props[key].value = styleMap[key];
+			} else {
+				props[key].value = '';
+			}
 		} else if (config[key].type === 'px') {
 			if (key.startsWith('boxShadow')) return;
-			let val = styleMap[key];
-			props[key].value = safePx(val, config[key].value);
+			if (styleMap[key] !== undefined) {
+				props[key].value = safePx(styleMap[key], 0);
+			} else {
+				props[key].value = '';
+			}
 		}
 	});
 
     let boxShadow = styleMap['box-shadow'];
-	if (boxShadow) {
+	if (boxShadow && boxShadow !== 'none') {
 		const parts = boxShadow.split(' ');
-		props.boxShadowX.value = safePx(parts[0], config.boxShadowX.value);
-		props.boxShadowY.value = safePx(parts[1], config.boxShadowY.value);
-		props.boxShadowBlur.value = safePx(parts[2], config.boxShadowBlur.value);
-		let color = parts[3] || config.boxShadowColor.value;
+		props.boxShadowX.value = safePx(parts[0], 0);
+		props.boxShadowY.value = safePx(parts[1], 0);
+		props.boxShadowBlur.value = safePx(parts[2], 0);
+		let color = parts[3] || '';
 		if (color && color.startsWith('rgb')) color = rgbToHex(color);
 		props.boxShadowColor.value = color;
 	} else {
-		props.boxShadowX.value = config.boxShadowX.value;
-		props.boxShadowY.value = config.boxShadowY.value;
-		props.boxShadowBlur.value = config.boxShadowBlur.value;
-		props.boxShadowColor.value = config.boxShadowColor.value;
+		props.boxShadowX.value = '';
+		props.boxShadowY.value = '';
+		props.boxShadowBlur.value = '';
+		props.boxShadowColor.value = '';
 	}
 	return props;
 }
@@ -85,26 +96,33 @@ function parseInitialStyles(styleString, config = cssConfig) {
 function applyCssProps(element, props, config = cssConfig) {
 	Object.keys(config).forEach(key => {
 		const prop = config[key];
-		const val = props[key]?.value ?? prop.value;
+		const val = props[key]?.value;
 		
 		if (key.startsWith('boxShadow')) {
 			return;
 		}
 		
 		if (prop.type === 'color') {
-			if (val) element.style.setProperty(key, val);
+			if (val !== '' && val !== undefined && val !== null) {
+				element.style.setProperty(key, val);
+			}
 		} else if (prop.type === 'px') {
-			if (val !== '' && val !== undefined) {
+			if (val !== '' && val !== undefined && val !== null) {
 				element.style.setProperty(key, val + 'px');
 			}
 		} else if (prop.type === 'dropdown') {
-			if (val) element.style.setProperty(key, val);
+			if (val !== '' && val !== undefined && val !== null && val !== 'default') {
+				element.style.setProperty(key, val);
+			}
 		}
 	});
 
-	const bx = props.boxShadowX?.value ?? config.boxShadowX.value;
-	const by = props.boxShadowY?.value ?? config.boxShadowY.value;
-	const blur = props.boxShadowBlur?.value ?? config.boxShadowBlur.value;
-	const color = props.boxShadowColor?.value ?? config.boxShadowColor.value;
-	element.style.setProperty('box-shadow', `${bx||0}px ${by||0}px ${blur||0}px ${color}`);
+	const bx = props.boxShadowX?.value;
+	const by = props.boxShadowY?.value;
+	const blur = props.boxShadowBlur?.value;
+	const color = props.boxShadowColor?.value;
+	
+	if (bx !== '' && bx !== undefined && bx !== null) {
+		element.style.setProperty('box-shadow', `${bx||0}px ${by||0}px ${blur||0}px ${color||'#000000'}`);
+	}
 }
